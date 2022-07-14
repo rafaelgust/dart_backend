66231c5d191366566d449429b24169131c786803
rafaelgust <rafael.appdevs@gmail.com>
2022-07-14 00:03:06 -0300

    routes with shelf router

--- bin/dart_backend.dart
+++ bin/dart_backend.dart
@@ -1,17 +1,5 @@
 import 'package:dart_backend/dart_backend.dart' as dart_backend;
 
-import 'package:shelf/shelf.dart';
-import 'package:shelf/shelf_io.dart' as shelf_io;
-
-Future<void> main() async {
-  var handler =
-      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
-
-  var server = await shelf_io.serve(handler, '0.0.0.0', 8080);
-
-  print('Hello world: ${dart_backend.calculate()}!');
-  print('Serving at http://${server.address.host}:${server.port}');
+void main() {
+  dart_backend.server();
 }
-
-Response _echoRequest(Request request) =>
-    Response.ok('Request for "${request.url}"');
--- lib/dart_backend.dart
+++ lib/dart_backend.dart
@@ -1,3 +1,54 @@
-int calculate() {
-  return 6 * 7;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
+
+var router = Router();
+
+final html = ''' 
+  <html>
+    <head>
+      <title>Dart Back-End</title>
+    </head>
+    <body>
+      <h1>Dart Back-End com shelf</h1>
+      <p>
+        u.u
+      </p>
+    </body>
+  </html>
+''';
+
+final json = '''
+  {
+    "message" : "Tudo Funfando"
+  }
+''';
+
+void server() async {
+  var handler = const Pipeline().addMiddleware(logRequests());
+
+  router.get('/home', handler.addHandler(_echoRequest));
+
+  router.get('/json', handler.addHandler(_echoRequestJson));
+
+  router.get(
+      '/hello', handler.addHandler((request) => Response.ok('hello-world')));
+
+  var server = await shelf_io.serve(router, '0.0.0.0', 8080);
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) => Response.ok(
+      html,
+      headers: {
+        'content-type': 'text/html',
+      },
+    );
+
+Response _echoRequestJson(Request request) => Response.ok(
+      json,
+      headers: {
+        'content-type': 'text/json',
+      },
+    );
