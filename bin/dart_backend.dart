5d007af455813e07f27964908275a53627a40228
rafaelgust <rafael.appdevs@gmail.com>
2022-07-13 23:33:16 -0300

    shelf => server working 8080

--- bin/dart_backend.dart
+++ bin/dart_backend.dart
@@ -1,5 +1,17 @@
 import 'package:dart_backend/dart_backend.dart' as dart_backend;
 
-void main(List<String> arguments) {
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+Future<void> main() async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  var server = await shelf_io.serve(handler, '0.0.0.0', 8080);
+
   print('Hello world: ${dart_backend.calculate()}!');
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
